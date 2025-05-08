0bb0998e4736a18bdbc05d80bd1dccd31c69ecb6
kosta-lacosta <konstantine1996@gmail.com>
2025-05-08 14:51:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function getAccessToken() {
           refresh_token: process.env.REFRESH_TOKEN,
           client_id: process.env.CLIENT_ID,
           client_secret: process.env.CLIENT_SECRET,
-          grant_type: 'refresh_token' // <--- исправлено
+          grant_type: refresh_token
         }
       }
     );
